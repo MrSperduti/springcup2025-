5b152fc401df93c3136efeb104d6cc75a4b836a8
MrSperduti <gianluca.sperduti@sctech.info>
2025-04-25 14:28:39 +0200

    Add files via upload

--- categoria.js
+++ categoria.js
@@ -1,18 +1,20 @@
-
 function getParam(name) {
   return new URLSearchParams(location.search).get(name);
 }
+
 document.addEventListener('DOMContentLoaded', () => {
   const categoria = getParam('categoria');
   document.getElementById('categoria-titolo').textContent = categoria;
   const menu = document.getElementById('menu');
+
   [
     ['Calendario', 'calendario.html'],
     ['Classifica', 'classifica.html'],
+    ['Gironi', 'gironi.html'],
+    ['Rose', 'rose.html'],
     ['Marcatori', 'classifica-marcatori.html'],
-    ['Classifica Miglior Portiere', 'classifica-portieri.html'],
     ['Classifica Miglior Giocatore', 'classifica-giocatori.html'],
-    ['Gironi', 'girone.html']
+    ['Classifica Miglior Portiere', 'classifica-portieri.html']
   ].forEach(([nome, file]) => {
     const btn = document.createElement('button');
     btn.textContent = nome;
