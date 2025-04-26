b02692d60cb6fa2d54799d32ad708c805769f936
MrSperduti <gianluca.sperduti@sctech.info>
2025-04-26 21:58:44 +0200

    Add files via upload

--- editor_regolamento.js
+++ editor_regolamento.js
@@ -2,7 +2,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   const pdfInput = document.getElementById("pdfInput");
   const uploadFileButton = document.getElementById("uploadFileButton");
-  const generateJsonButton = document.getElementById("generateJsonButton");
   let fileDetails = null;
 
   // Funzione per caricare il file e aggiungere alla tabella
@@ -21,43 +20,30 @@ document.addEventListener("DOMContentLoaded", function() {
         type: file.type,
         size: file.size
       };
-      
-      // Mostra il bottone per generare il JSON
-      generateJsonButton.style.display = 'inline-block';
-    } else {
-      alert('Seleziona un file da caricare');
-    }
-  });
 
-  // Funzione per generare il JSON e scaricarlo
-  generateJsonButton.addEventListener('click', function() {
-    if (fileDetails) {
-      // Caricare il file JSON esistente dal localStorage o crearne uno nuovo
+      // Aggiungi il file al JSON esistente nel localStorage
       let existingFiles = [];
       if (localStorage.getItem('regolamentoFiles')) {
         existingFiles = JSON.parse(localStorage.getItem('regolamentoFiles'));
       }
-
-      // Aggiungi il nuovo file alla lista esistente
       existingFiles.push(fileDetails);
-
-      // Salva la lista aggiornata dei file nel localStorage
       localStorage.setItem('regolamentoFiles', JSON.stringify(existingFiles));
 
-      // Crea un file JSON con i dettagli dei file
+      // Crea il file JSON per il download
       const jsonBlob = new Blob([JSON.stringify({regolamentoFiles: existingFiles})], {type: 'application/json'});
       const jsonURL = URL.createObjectURL(jsonBlob);
-      
+
       // Crea un link per scaricare il JSON
       const downloadLink = document.createElement('a');
       downloadLink.href = jsonURL;
       downloadLink.download = 'regolamentoFiles.json';
       downloadLink.textContent = 'Scarica il JSON generato';
-      
+
       // Aggiungi il link al body o dove vuoi
       document.body.appendChild(downloadLink);
+
     } else {
-      alert('Nessun file caricato per generare il JSON');
+      alert('Seleziona un file da caricare');
     }
   });
 });
