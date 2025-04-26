ba04f293bc17f4f547435346e32e9860b8d639c1
MrSperduti <gianluca.sperduti@sctech.info>
2025-04-27 00:19:46 +0200

    Add files via upload

--- regolamento.js
+++ regolamento.js
@@ -2,9 +2,11 @@
 document.addEventListener("DOMContentLoaded", function() {
   const tableBody = document.querySelector('#pdfTable tbody');
 
+  // Carica i file JSON dal repository
   fetch('https://raw.githubusercontent.com/MrSperduti/springcup2025-/main/regolamentoFiles.json')
     .then(response => response.json())
     .then(data => {
+      // Aggiungi i file alla tabella
       data.regolamentoFiles.forEach(file => {
         const row = document.createElement('tr');
         const nameCell = document.createElement('td');
